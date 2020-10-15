d4ed1d6292af6e72bb9d428cc3fb83fb214e3cf1
hppRC <hpp.ricecake@gmail.com>
2020-10-16 00:15:45 +0900

    :up: Update package

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
   <title>Sapper project template</title>
 </svelte:head>
 
-<h1 class="">Great success!</h1>
+<h1 class="text-4xl font-bold">Great success!</h1>
 
 <figure>
   <img alt="Icon" src="images/icon.png" />
